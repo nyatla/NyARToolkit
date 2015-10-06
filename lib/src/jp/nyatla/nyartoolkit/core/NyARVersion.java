a3a286f12c43db3b35e729987f83920eec30cf68
nyatla <nyatla39@gmail.com>
2015-10-06 12:38:50 +0900

    revert version number

--- lib/src/jp/nyatla/nyartoolkit/core/NyARVersion.java
+++ lib/src/jp/nyatla/nyartoolkit/core/NyARVersion.java
@@ -40,9 +40,9 @@ public class NyARVersion
 	/**メジャーバージョン*/
 	public final static int VERSION_MAJOR=4;
 	/**マイナバージョン*/
-	public final static int VERSION_MINOR=1;
+	public final static int VERSION_MINOR=2;
 	/**タグ*/
-	public final static int VERSION_TAG=2;
+	public final static int VERSION_TAG=1;
 	/**バージョン文字列*/
 	public final static String VERSION_STRING=MODULE_NAME+"/"+VERSION_MAJOR+"."+VERSION_MINOR+"."+VERSION_TAG;
 }
