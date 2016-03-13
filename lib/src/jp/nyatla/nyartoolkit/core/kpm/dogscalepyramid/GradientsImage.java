42cbe16aeb1f2953eaa71af6825e5a7bd6fb8573
nyatla <nyatla39@gmail.com>
2016-03-13 21:22:28 +0900

    GradientsImageの最適化

--- lib/src/jp/nyatla/nyartoolkit/core/kpm/dogscalepyramid/GradientsImage.java
+++ lib/src/jp/nyatla/nyartoolkit/core/kpm/dogscalepyramid/GradientsImage.java
@@ -1,67 +1,269 @@
 package jp.nyatla.nyartoolkit.core.kpm.dogscalepyramid;
 
 import jp.nyatla.nyartoolkit.core.NyARRuntimeException;
+import jp.nyatla.nyartoolkit.core.kpm.KpmImage;
+import jp.nyatla.nyartoolkit.core.math.NyARMath;
 import jp.nyatla.nyartoolkit.core.raster.INyARRaster;
 import jp.nyatla.nyartoolkit.core.types.NyARBufferType;
 import jp.nyatla.nyartoolkit.core.types.NyARIntSize;
 
-public class GradientsImage implements INyARRaster {
-	private NyARIntSize _size;
-	private double[] _buf;
+public class GradientsImage
+{
+	final private static double PI=NyARMath.PI;
+	final private NyARIntSize _size;
+//	final private double[] _buf;
+	final private double[] _angle;
+	final private double[] _mag;
 
 	public GradientsImage(int i_width, int i_height) {
 		this._size = new NyARIntSize(i_width, i_height);
-		this._buf = new double[i_width * i_height * 2];
+//		this._buf = new double[i_width * i_height * 2];
+		this._angle = new double[i_width * i_height];
+		this._mag = new double[i_width * i_height];
 	}
 
-	@Override
 	public int getWidth() {
 		return this._size.w;
 	}
 
-	@Override
 	public int getHeight() {
 		return this._size.h;
 	}
 
-	@Override
+
 	public NyARIntSize getSize() {
 		return this._size;
 	}
 
-	@Override
-	public Object getBuffer() {
-		return this._buf;
-	}
 
-	@Override
+
+
 	public int getBufferType() {
 		return NyARBufferType.USER_DEFINE;
 	}
 
-	@Override
+
 	public boolean isEqualBufferType(int i_type_value) {
 		return false;
 	}
 
-	@Override
+
 	public boolean hasBuffer() {
 		return true;
 	}
 
-	@Override
+
 	public void wrapBuffer(Object i_ref_buf) {
 		NyARRuntimeException.notImplement();
 	}
 
-	@Override
+
 	public Object createInterface(Class<?> i_iid) {
 		throw new NyARRuntimeException();
 	}
 
-	// これどうにかしよう
-	public int get(int i_row) {
-		return this._size.w * i_row * 2;
-	}
 
+	/**
+	 * OrientationAssignment.computeの一部
+	 * @param x
+	 * @param y
+	 * @param radius
+	 * @param gw_scale
+	 * @param i_histogram
+	 */
+	public void buildOrientationHistogram(double x,double y,double radius,double gw_scale,BilinearHistogram i_histogram)
+	{
+		double[] a_buf=this._angle;
+		double[] m_buf=this._mag;
+		int xi = (int) (x + 0.5f);
+		int yi = (int) (y + 0.5f);		
+		// Box around feature point
+		int x0 = xi - (int) (radius + 0.5f);
+		int x1 = xi + (int) (radius + 0.5f);
+		int y0 = yi - (int) (radius + 0.5f);
+		int y1 = yi + (int) (radius + 0.5f);
+
+		// Clip the box to be within the bounds of the image
+		int width_1=this._size.w - 1;
+		int height_1=this._size.h - 1;
+		if(x0<0){ x0=0;}//x0 = math_utils.max2(0, x0);
+		if(x1>width_1){x1=width_1;}//x1 = math_utils.min2(x1, (int) g.getWidth() - 1);
+		if(y0<0){y0=0;}//y0 = math_utils.max2(0, y0);
+		if(y1>height_1){y1=height_1;}//y1 = math_utils.min2(y1, (int) g.getHeight() - 1);
+		
+		double radius2 = Math.ceil(radius*radius);
+		// Build up the orientation histogram
+		for (int yp = y0; yp <= y1; yp++) {
+			double dy = yp - y;
+			double dy2 = (dy*dy);
+
+			int y_ptr = this._size.w * yp;
+
+			for (int xp = x0; xp <= x1; xp++) {
+				double dx = xp - x;
+				double r2 = (dx*dx) + dy2;
+
+				// Only use the gradients within the circular window
+				if (r2 > radius2) {
+					continue;
+				}
+				int g2_ptr = y_ptr + xp; // const float* g = &y_ptr[xp<<1];
+				double angle = a_buf[g2_ptr];// const float& angle = g[0];
+				double mag = m_buf[g2_ptr];// const float& mag = g[1];
+
+				// Compute the gaussian weight based on distance from center of keypoint
+				double w = fastexp6(r2 * gw_scale);
+
+
+				// Vote to the orientation histogram with a bilinear update
+				i_histogram.bilinearHistogramUpdate(angle,w * mag);
+			}
+		}		
+	}
+	public void computePolarGradients(KpmImage i_img)
+	{
+		double dx, dy;
+		double[] a_gradient=this._angle;
+		double[] m_gradient=this._mag;
+		int width=this._size.w;
+		int height=this._size.h;
+		assert this._size.isEqualSize(null);
+		double[] im=(double[])i_img.getBuffer();
+
+
+		int width_minus_1;
+		int height_minus_1;
+
+		int p_ptr;
+		int pm1_ptr;
+		int pp1_ptr;
+
+		width_minus_1 = width - 1;
+		height_minus_1 = height - 1;
+		int gradient_ptr = 0;
+
+		// Top row
+		pm1_ptr = 0; // pm1_ptr = im;
+		p_ptr = 0; // p_ptr = im;
+		pp1_ptr = width;// pp1_ptr = p_ptr+width;
+
+		dx = im[p_ptr + 1] - im[p_ptr + 0];// dx = p_ptr[1] - p_ptr[0];
+		dy = im[pp1_ptr + 0] - im[pm1_ptr + 0];// dy = pp1_ptr[0] - pm1_ptr[0];
+		// SET_GRADIENT(dx, dy)
+		a_gradient[gradient_ptr] = (Math.atan2(dy, dx) + PI);
+		m_gradient[gradient_ptr] = Math.sqrt(dx * dx + dy * dy);
+		gradient_ptr++;
+		p_ptr++;
+		pm1_ptr++;
+		pp1_ptr++;
+
+		for (int col = 1; col < width_minus_1; col++) {
+			dx = im[p_ptr + 1] - im[p_ptr - 1];
+			dy = im[pp1_ptr + 0] - im[pm1_ptr + 0];
+			// SET_GRADIENT(dx, dy)
+			a_gradient[gradient_ptr] = (Math.atan2(dy, dx) + PI);
+			m_gradient[gradient_ptr] = Math.sqrt(dx * dx + dy * dy);
+			gradient_ptr++;
+			p_ptr++;
+			pm1_ptr++;
+			pp1_ptr++;
+		}
+
+		dx = im[p_ptr + 0] - im[p_ptr - 1];
+		dy = im[pp1_ptr + 0] - im[pm1_ptr + 0];
+		// SET_GRADIENT(dx, dy)
+		a_gradient[gradient_ptr] = (Math.atan2(dy, dx) + PI);
+		m_gradient[gradient_ptr] = Math.sqrt(dx * dx + dy * dy);
+		gradient_ptr++;
+		p_ptr++;
+		pm1_ptr++;
+		pp1_ptr++;
+
+		// Non-border pixels
+		pm1_ptr = 0;// pm1_ptr = im;
+		p_ptr = pm1_ptr + width;
+		pp1_ptr = p_ptr + width;
+
+		for (int row = 1; row < height_minus_1; row++) {
+			dx = im[p_ptr + 1] - im[p_ptr + 0];
+			dy = im[pp1_ptr + 0] - im[pm1_ptr + 0];
+			// SET_GRADIENT(dx, dy)
+			a_gradient[gradient_ptr] = (Math.atan2(dy, dx) + PI);
+			m_gradient[gradient_ptr] = Math.sqrt(dx * dx + dy * dy);
+			gradient_ptr++;
+			p_ptr++;
+			pm1_ptr++;
+			pp1_ptr++;
+
+			for (int col = 1; col < width_minus_1; col++) {
+				dx = im[p_ptr + 1] - im[p_ptr - 1];
+				dy = im[pp1_ptr + 0] - im[pm1_ptr + 0];
+				// SET_GRADIENT(dx, dy)
+				a_gradient[gradient_ptr] = (Math.atan2(dy, dx) + PI);
+				m_gradient[gradient_ptr] = Math.sqrt(dx * dx + dy * dy);
+				gradient_ptr++;
+				p_ptr++;
+				pm1_ptr++;
+				pp1_ptr++;
+			}
+			dx = im[p_ptr + 0] - im[p_ptr - 1];
+			dy = im[pp1_ptr + 0] - im[pm1_ptr + 0];
+			// SET_GRADIENT(dx, dy)
+			a_gradient[gradient_ptr] = (Math.atan2(dy, dx) + PI);
+			m_gradient[gradient_ptr] = Math.sqrt(dx * dx + dy * dy);
+			gradient_ptr++;
+			p_ptr++;
+			pm1_ptr++;
+			pp1_ptr++;
+		}
+
+		// Lower row
+		p_ptr = height_minus_1 * width;// p_ptr = &im[height_minus_1*width];
+		pm1_ptr = p_ptr - width;
+		pp1_ptr = p_ptr;
+
+		dx = im[p_ptr + 1] - im[p_ptr + 0];
+		dy = im[pp1_ptr + 0] - im[pm1_ptr + 0];
+		// SET_GRADIENT(dx, dy)
+		a_gradient[gradient_ptr] = (Math.atan2(dy, dx) + PI);
+		m_gradient[gradient_ptr] = Math.sqrt(dx * dx + dy * dy);
+		gradient_ptr++;
+		p_ptr++;
+		pm1_ptr++;
+		pp1_ptr++;
+
+		for (int col = 1; col < width_minus_1; col++) {
+			dx = im[p_ptr + 1] - im[p_ptr - 1];
+			dy = im[pp1_ptr + 0] - im[pm1_ptr + 0];
+			// SET_GRADIENT(dx, dy)
+			a_gradient[gradient_ptr] = (Math.atan2(dy, dx) + PI);
+			m_gradient[gradient_ptr] = Math.sqrt(dx * dx + dy * dy);
+			gradient_ptr++;
+			p_ptr++;
+			pm1_ptr++;
+			pp1_ptr++;
+
+		}
+
+		dx = im[p_ptr + 0] - im[p_ptr - 1];
+		dy = im[pp1_ptr + 0] - im[pm1_ptr + 0];
+		// SET_GRADIENT(dx, dy)
+		a_gradient[gradient_ptr] = (Math.atan2(dy, dx) + PI);
+		m_gradient[gradient_ptr] = Math.sqrt(dx * dx + dy * dy);
+		gradient_ptr++;
+		p_ptr++;
+		pm1_ptr++;
+		pp1_ptr++;
+	}	
+	
+    /**
+     * 0.01% error at 1.030
+     * 0.10% error at 1.520
+     * 1.00% error at 2.330
+     * 5.00% error at 3.285
+     */
+    final private static double fastexp6(double x) {
+        return (720+x*(720+x*(360+x*(120+x*(30+x*(6+x))))))*0.0013888888;
+    }		
+	
 }
