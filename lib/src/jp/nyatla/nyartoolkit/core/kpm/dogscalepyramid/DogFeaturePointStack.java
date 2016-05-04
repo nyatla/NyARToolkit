c4d338ccc90b08900fe894ac6c01eec7b07bc2c9
nyatla <nyatla39@gmail.com>
2016-05-04 20:52:44 +0900

    不要なinterfaceを削除

--- lib/src/jp/nyatla/nyartoolkit/core/kpm/dogscalepyramid/DogFeaturePointStack.java
+++ lib/src/jp/nyatla/nyartoolkit/core/kpm/dogscalepyramid/DogFeaturePointStack.java
@@ -37,7 +37,8 @@ package jp.nyatla.nyartoolkit.core.kpm.dogscalepyramid;
 
 import jp.nyatla.nyartoolkit.core.types.stack.NyARObjectStack;
 
-public class DogFeaturePointStack extends NyARObjectStack<DogFeaturePoint> implements Cloneable {
+public class DogFeaturePointStack extends NyARObjectStack<DogFeaturePoint>
+{
 	public DogFeaturePointStack(int i_length) {
 		super(i_length, DogFeaturePoint.class);
 	}
