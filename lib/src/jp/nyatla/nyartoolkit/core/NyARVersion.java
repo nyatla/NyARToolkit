1361a326ff7b37bf4beff82bd5e9a80d94cf8975
nyatla <nyatla39@gmail.com>
2016-05-24 00:22:02 +0900

    update version

--- lib/src/jp/nyatla/nyartoolkit/core/NyARVersion.java
+++ lib/src/jp/nyatla/nyartoolkit/core/NyARVersion.java
@@ -42,7 +42,7 @@ public class NyARVersion
 	/**マイナバージョン*/
 	public final static int VERSION_MINOR=0;
 	/**タグ*/
-	public final static int VERSION_TAG=8;
+	public final static int VERSION_TAG=9;
 	/**バージョン文字列*/
 	public final static String VERSION_STRING=MODULE_NAME+"/"+VERSION_MAJOR+"."+VERSION_MINOR+"."+VERSION_TAG;
 }
